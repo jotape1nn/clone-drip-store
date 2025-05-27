ddb5a33b7ce93217b80ed8a609bce50e47717baa
jotape1nn <joaopaulolimamourafju@gmail.com>
2025-05-27 14:27:39 -0300

    refatoraçao

--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import Logo from '../Logo/Logo'
-import './Header.css';
-export default function Header(){
-  return(
-    <header>
-      <Logo src={'../../../public/logo.svg'} cor={'#c92071'}/>
-    </header>
-  )
-}
